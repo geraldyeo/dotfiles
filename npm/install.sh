951b1eaf0281ece504cfd356209cabde16175528
Gerald Yeo <gerald.yeo@gmail.com>
2016-10-06 10:58:31 +0800

    .

--- npm/install.sh
+++ npm/install.sh
@@ -13,8 +13,9 @@ run_npm () {
 	  #local packages="npm npm-check iron-node xo eslint babel-cli karma-cli grunt-cli gulp-cli create-react-project yo browser-sync"
 	  #npm i -g $packages
 	  npm i -g npm npm-check license-checker nsp snyk retire \
-	  	trash-cli babel-cli karma-cli grunt-cli gulp-cli react-native-cli create-react-app plugman nodal \
-		eslint stylelint stylint \
+	  	trash-cli babel-cli karma-cli grunt-cli gulp-cli react-native-cli create-react-app flow-typed plugman nodal lerna yo \
+		eslint babel-eslint eslint-plugin-react eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-flowtype \
+		stylelint stylint \
 		vorlon browser-sync bunyan pm2
 	fi
 }
