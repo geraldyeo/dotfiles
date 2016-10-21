8311edad624e8707c544c973a39270f5d2592324
Gerald Yeo <gerald.yeo@gmail.com>
2016-10-21 18:49:04 +0800

    update gitconfig

--- npm/install.sh
+++ npm/install.sh
@@ -10,13 +10,11 @@ run_npm () {
 	then
 	  echo "  Installing npm packages for you."
 
-	  #local packages="npm npm-check iron-node xo eslint babel-cli karma-cli grunt-cli gulp-cli create-react-project yo browser-sync"
-	  #npm i -g $packages
 	  npm i -g npm npm-check license-checker nsp snyk retire \
-	  	trash-cli babel-cli karma-cli grunt-cli gulp-cli react-native-cli create-react-app flow-typed plugman nodal lerna yo \
+	  	trash-cli babel-cli karma-cli grunt-cli gulp-cli semantic-release-cli react-native-cli create-react-app flow-typed plugman nodal lerna yo \
 		eslint babel-eslint eslint-plugin-react eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-flowtype \
 		stylelint stylint \
-		vorlon browser-sync bunyan pm2
+		vorlon browser-sync bunyan pm2 lighthouse
 	fi
 }
 
