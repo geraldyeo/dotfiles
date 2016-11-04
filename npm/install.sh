b04599b80eab2dae8ccb530fb83ea8f581681e7c
Gerald Yeo <gerald.yeo@gmail.com>
2016-11-04 17:20:47 +0800

    update npm deps

--- npm/install.sh
+++ npm/install.sh
@@ -10,11 +10,12 @@ run_npm () {
 	then
 	  echo "  Installing npm packages for you."
 
-	  npm i -g npm npm-check license-checker nsp snyk retire \
-	  	trash-cli babel-cli karma-cli grunt-cli gulp-cli semantic-release-cli react-native-cli create-react-app flow-typed plugman nodal lerna yo \
-		eslint babel-eslint eslint-plugin-react eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-flowtype \
-		stylelint stylint \
-		vorlon browser-sync bunyan pm2 lighthouse
+	  npm i -g npm yarn npm-check license-checker nsp snyk retire \
+			commitizen cz-conventional-changelog \
+		  trash-cli babel-cli karma-cli grunt-cli gulp-cli semantic-release-cli react-native-cli create-react-app flow-typed plugman nodal lerna yo \
+			eslint babel-eslint eslint-plugin-react eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-flowtype \
+			stylelint stylint \
+			vorlon browser-sync bunyan pm2 lighthouse
 	fi
 }
 
